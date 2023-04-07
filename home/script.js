16a5f23bd69e1d732ad6b1d6e1fd5597c5381bd8
Laura Yien <lnyien@ucdavis.edu>
2023-04-07 00:39:22 -0700

    most changes for this

--- home/script.js
+++ home/script.js
@@ -7,33 +7,51 @@
     const sections = document.querySelectorAll('section');
     const h1 = document.querySelector("h1");
     const h2 = document.querySelector("h2");
-    const h3 = document.querySelectorAll("h3")
-    let mode = 'dark';
+    const h3s = document.querySelectorAll("h3")
+    const main = document.querySelector("main");
+    let mode = 'light';
 
     button.addEventListener('click', function() {
-        if (mode === 'dark') {
+        if (mode === 'light') {
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
             h1.className = 'switch';
             h2.className = 'switch';
-            h3.className = 'switch';
+            main.className = 'switch';
+            banner.innerHTML = `<img src="images/color.png" alt="decorative image">`;
+            button.innerHTML = `monochrome`;
+
+            
+            for (const h3 of h3s) {
+                h3.className = 'switch';
+            }
+
             for (const section of sections) {
                 section.className = 'switch';
-            }
-            mode = 'light';
+            } 
+
+            mode = 'dark';
+
         } else {
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
             h1.removeAttribute('class');
             h2.removeAttribute('class');
-            h3.removeAttribute('class');
+            main.removeAttribute('class');
+            banner.innerHTML = `<img src="images/ocean.png" alt="image of the ocean">`
+            button.innerHTML = `Color`;
+
 
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+
+            for (const h3 of h3s) {
+                h3.removeAttribute('class');
+            }
+            mode = 'light'
         }
     })
 })()
