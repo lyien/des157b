580c0e926151027e6e42b507310ba24faabcf47e
Laura Yien <lnyien@ucdavis.edu>
2023-04-07 00:18:47 -0700

    styling for home page

--- home/script.js
+++ home/script.js
@@ -4,7 +4,10 @@
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const sections = document.querySelectorAll('section');
+    const h1 = document.querySelector("h1");
+    const h2 = document.querySelector("h2");
+    const h3 = document.querySelectorAll("h3")
     let mode = 'dark';
 
     button.addEventListener('click', function() {
@@ -12,6 +15,9 @@
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
+            h1.className = 'switch';
+            h2.className = 'switch';
+            h3.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
@@ -20,6 +26,10 @@
             body.removeAttribute('class');
             banner.removeAttribute('class');
             button.removeAttribute('class');
+            h1.removeAttribute('class');
+            h2.removeAttribute('class');
+            h3.removeAttribute('class');
+
             for (const section of sections) {
                 section.removeAttribute('class');
             }
