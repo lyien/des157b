daf9d78d4dab1919d399eb0d55ea2c6b4648cff4
Laura Yien <lnyien@ucdavis.edu>
2023-04-27 15:22:16 -0700

    reformatted

--- studio2/script.js
+++ studio2/script.js
@@ -9,7 +9,6 @@
     const dayimg = document.querySelector("#dayimg");
     document.querySelector("#daybutton").innerHTML = createButton(data);
     createEvents();
-    // dayimg.innerHTML = createText(data);
     }
 
 
@@ -46,63 +45,6 @@
     
         document.querySelector('#dayimg').innerHTML = text;
     }
-
-
-
-
-
-
-    // function createText(data){
-    //      document.querySelector("#Sunday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Sunday.date}</h2>
-    //             <p>${data.Sunday.time}</p>
-    //             <img src="images/${data.Sunday.img}" alt="Laura and friends at boba" width="325" height="401">
-    //             <p>${data.Sunday.location}</p>`;
-    //         })
-    //     document.querySelector("#Monday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Monday.date}</h2>
-    //             <p>${data.Monday.time}</p>
-    //             <img src="images/${data.Monday.img}" alt="Laura eating a crepe with her roommate" width="325" height="401">
-    //             <p>${data.Monday.location}</p>`;
-    //         })
-    //     document.querySelector("#Tuesday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Tuesday.date}</h2>
-    //             <p>${data.Tuesday.time}</p>
-    //             <img src="images/${data.Tuesday.img}" alt="Laura and her roommate at a glass bead making class" width="325" height="401">
-    //             <p>${data.Tuesday.location}</p>`;
-    //         })
-    //     document.querySelector("#Wednesday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Wednesday.date}</h2>
-    //             <p>${data.Wednesday.time}</p>
-    //             <img src="images/${data.Wednesday.img}" alt="Laura in class" width="325" height="401">
-    //             <p>${data.Wednesday.location}</p>`;
-    //         })
-    //     document.querySelector("#Thursday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Thursday.date}</h2>
-    //             <p>${data.Thursday.time}</p>
-    //             <img src="images/${data.Thursday.img}" alt="Laura eating dinner from Nick the Greek" width="325" height="401">
-    //             <p>${data.Thursday.location}</p>`;
-    //         })
-    //     document.querySelector("#Friday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Friday.date}</h2>
-    //             <p>${data.Friday.time}</p>
-    //             <img src="images/${data.Friday.img}" alt="Laura got her ears pierced!" width="325" height="401">
-    //             <p>${data.Friday.location}</p>`;
-    //         })
-    //     document.querySelector("#Saturday").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Saturday.date}</h2>
-    //             <p>${data.Saturday.time}</p>
-    //             <img src="images/${data.Saturday.img}" alt="Laura at on Picnic Day at a party" width="325" height="401">
-    //             <p>${data.Saturday.location}</p>`;
-    //         })
-    //     document.querySelector("#Recap").addEventListener("click",function(){
-    //             let imgInfo = dayimg.innerHTML = `<h2>${data.Recap.date}</h2>
-    //             <p>${data.Recap.time}</p>
-    //             <img src="images/${data.Recap.img}" alt="Laura and her friends eating dinner" width="325" height="401">
-    //             <p>${data.Recap.location}</p>`;
-    //         })
-    // }
-
 
     getData();
 })();
